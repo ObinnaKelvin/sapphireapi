2c78593982fdb84a67930d463d78db312b42c739
Obinna Okere <okereobinna10@gmail.com>
2023-11-23 17:18:26 +0100

    Service Schema Created

---
+++ models/Service.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const ServiceSchema = new Schema ({
+    serviceId: {
+        type: Number
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    code: {
+        type: Number
+    },
+    standardPrice: {
+        type: Number
+    },
+    active: {
+        type: Number
+    },
+    encodedBy: {
+        type: Number
+    },
+    encodedDate: {
+        type: Date,
+        required: true
+    },
+    lastUpdatedBy: {
+        type: Number,
+        required: true
+    },
+    lastUpdatedDate: {
+        type: Date,
+        required: true
+    },
+}, {timestamps: true})
+
+export default model("Service", ServiceSchema)
