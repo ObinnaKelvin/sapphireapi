447e8aec1748180aadb25698799fe70b53990deb
Obinna Okere <okereobinna10@gmail.com>
2023-11-23 17:08:21 +0100

    Appointment model and schema created

---
+++ models/Appointment.js
@@ -0,0 +1,109 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const AppointmentSchema = new Schema ({
+    appointmentId: {
+        type: Number
+    },
+    patientId: {
+        type: Number
+    },
+    patientNo: {
+        type: Number
+    },
+    staffId: {
+        type: Number
+    },
+    visitId: {
+        type: Number
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    middleName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    age: {
+        type: Date,
+        required: true
+    },
+    genderId: {
+        type: Number,
+        required: true
+    },
+    mobile: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    referralTypeId: {
+        type: Number
+    },
+    referToDoctor: {
+        type: Number
+    },
+    authorizationNo: {
+        type: Number
+    },
+    appointmentStatus: {
+        type: Number
+    },
+    paymentStatus: {
+        type: Number
+    },
+    active: {
+        type: Number
+    },
+    encodedBy: {
+        type: Number,
+        required: true
+    },
+    encodedDate: {
+        type: Date,
+        required: true
+    },
+    lastUpdatedBy: {
+        type: Number,
+        required: true
+    },
+    lastUpdatedDate: {
+        type: Date,
+        required: true
+    },
+    cancelReason: {
+        type: String
+    },
+    // invoiceId integer
+    payerId: {
+        type: Number,
+        required: true
+    },
+    payerCategoryId: {
+        type: Number,
+        required: true
+    },
+    checkInBy: {
+        type: Number,
+        required: true
+    },
+    checkInDate: {
+        type: Date,
+        required: true
+    },
+}, {timestamps: true})
+
+export default model("Appointment", AppointmentSchema)
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+dotenv.config();
+mongoose.set('strictQuery', false);
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log(`🕹️ Connected to Sapphire Database!! 🕹️`)
+        
+    } catch (error) {
+        throw(error)
+    }
+}
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+
+ //Add Cors before the routers 
+app.use(cors(corsOptions));
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
+
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, ()=> {
+    connect()
+    console.log(`🚀 Server listening on port: ${PORT} 🚀`)
+})
