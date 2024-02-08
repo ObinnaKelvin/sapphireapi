08933df457861bbdf3b396d3a1f77c5efeeb3cfa
Obinna Okere <okereobinna10@gmail.com>
2024-02-08 12:32:42 +0100

    Cors origin changed to localhost:3000

--- src/index.js
+++ src/index.js
@@ -29,7 +29,8 @@ const connect = async () => {
 }
 
 const corsOptions ={
-    origin:'*', 
+    // origin:'*', 
+    origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
  }
